856faeb17908a3057dbab7619fc6ebb713cdb63c
YonahHR <1037538@hr.nl>
2023-05-16 10:20:57 +0200

    First push

---
+++ Assets/PlaceImage.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class PlaceImage : MonoBehaviour
+{
+    // Reference to AR tracked image manager component
+    private ARTrackedImageManager _trackedImagesManager;
+
+    // List of prefabs to instantiate - these should be named the same
+    // as their corresponding 2D images in the reference image library 
+    public GameObject[] ArPrefabs;
+
+
+    private readonly Dictionary<string, GameObject> _instantiatedPrefabs = new Dictionary<string, GameObject>();
+
+    void Awake()
+    {
+        _trackedImagesManager = GetComponent<ARTrackedImageManager>();
+    }
+    private void OnEnable()
+    {
+        _trackedImagesManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+    private void OnDisable()
+    {
+        _trackedImagesManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            // Get the name of the reference image
+            var imageName = trackedImage.referenceImage.name;
+            if (string.Compare("HRQR", imageName, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                Application.OpenURL("https://en.wikipedia.org/wiki/King_of_Clubs");
+            }
+            else if (string.Compare("k_spades", imageName, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                Application.OpenURL("https://en.wikipedia.org/wiki/King_of_Spades");
+            }
+        }
+
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            // Destroy its prefab
+            Destroy(_instantiatedPrefabs[trackedImage.referenceImage.name]);
+            // Also remove the instance from our array
+            _instantiatedPrefabs.Remove(trackedImage.referenceImage.name);
+            // Or, simply set the prefab instance to inactive
+            //_instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(false);
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            _instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
+        }
+    }
+}
